419586a0e29996d55ee94528dacc5234d1c62a7e
Elias Mari <elias.mari.p@gmail.com>
2024-02-05 18:22:37 +0100

    modificacion de idioma

--- main.dart
+++ main.dart
@@ -5,5 +5,5 @@ void main(List<String> arguments) {
     // Formatear la fecha y hora
     String formattedDateTime = "${now.year}-${now.month}-${now.day} ${now.hour}:${now.minute}:${now.second}";
     // Imprimir el mensaje con la fecha y hora
-    print('Hello world: ${dart_application_1.calculate()}! ($formattedDateTime)');
+    print('Hola mundo: ${dart_application_1.calculate()}! ($formattedDateTime)');
 }
